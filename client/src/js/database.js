5c546bbbc94c0a2e83456657a6573961b8a76c0d
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-25 19:02:18 -0500

    created guess code for db.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,6 @@
 import { openDB } from 'idb';
 
+// Initial route and start db
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,9 +14,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// POST route
+export const postDb = async (content) => {
+  console.log('POST to database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ content: content });
+  const result = await request;
+  console.log('Data saved to database: ', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from database');
+
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('Data retrieved from database: ', result);
+  return result;
+}
 
 initdb();
