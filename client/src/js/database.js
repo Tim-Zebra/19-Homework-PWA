5e5f4bd848bf63fb3bfbe5005cab5b241cb28162
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-27 15:29:39 -0500

    removed array option

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,7 +28,7 @@ export const getDb = async () => {
   console.log('GET from database');
 
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction(['jate'], 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
