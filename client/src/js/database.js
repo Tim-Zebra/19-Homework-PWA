dc83897a97d80ba9d1b4b94b66b3a9fc319b4949
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-27 15:38:45 -0500

    changed absolutely nothing, now service worker cannot be found....unsure how naming is so subjective in code compilers

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,15 +18,13 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ jate: content });
+  const request = store.put({ content: content });
   const result = await request;
   console.log('Data saved to database: ', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.log('GET from database');
-
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
