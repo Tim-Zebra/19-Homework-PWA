5445af2e9950ff8bb629074e69f55859983adb75
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-27 16:52:21 -0500

    had put method only apply to one id value. Prevents multiple objects.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,13 +18,14 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ content: content });
+  const request = store.put({ id: 1, content: content });
   const result = await request;
   console.log('Data saved to database: ', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
+
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
